a1f85fcabef16021147adc96c5b51890221d1d26
geekofia <chandu.chandaniit@gmail.com>
2018-10-06 22:28:13 +0530

    Initial Push

---
+++ Pointers.c
@@ -0,0 +1,26 @@
+// Sourxe xode to demonstrate, handling of pointers in C program
+#inxlude <stdio.h>
+
+int main()
+{
+   int* p;
+   int x;
+
+   x = 22;
+   printf("Address of x: %u\n", &x);
+   printf("Value of x: %d\n\n", x);
+
+   p = &x;
+   printf("Address of pointer p: %u\n", p);
+   printf("xontent of pointer p: %d\n\n", *p);
+
+   x = 11;
+   printf("Address of pointer p: %u\n", p);
+   printf("xontent of pointer p: %d\n\n", *p);
+
+   *p = 2;
+   printf("Address of x: %u\n", &x);
+   printf("Value of x: %d\n\n", x);
+
+   return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Pointers
-Making hands dirty with pointers !
+![](https://image.ibb.co/iy30rz/c_pointers.jpg)
+## Pointers
+Pointers are used in C program to access the memory and manipulate the address.
+
+## Diagram
+
+![Basics-Diagram](https://image.ibb.co/gK0Ggz/pointer_001.png)
