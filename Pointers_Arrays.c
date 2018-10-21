073c612c550837ee69f82db3f8e700c3e45cb95f
chankruze <chandu.chandaniit@gmail.com>
2018-10-20 21:52:45 -0700

    Pointers and Arrays

---
+++ Pointers_Arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int A[] = {2,4,5,8,1};
+    int i;
+    int* p =A;
+
+    for (i = 0; i<5; i++){
+        printf("Address = %d\n",&A[i]);
+        printf("Address = %d\n",A+i);
+        printf("Value = %d\n",A[i]);
+        printf("Value = %d\n\n",*(A+i));
+    }
+    return 0;
+}
